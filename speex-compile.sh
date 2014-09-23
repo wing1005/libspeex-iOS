d5457c53442e510eb730c6e77f16f0cd46ec3c2b
Evan JIANG <firstfan@gmail.com>
2014-09-24 00:04:29 +0800

    Update speex compile script to support arm64 x86_64

--- speex-compile.sh
+++ speex-compile.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 VERSION="1.2rc1"
-SDKVERSION="5.1"
+SDKVERSION="8.0"
 LIB="speex"
 
 DEVELOPER=`xcode-select -print-path`
-ARCHS="i386 armv6 armv7"
+ARCHS="i386 x86_64 armv7 arm64"
 CURRENTPATH=`pwd`
 BUILD="x86_64-apple-darwin11"
 OLD_PATH=$PATH
@@ -14,20 +14,26 @@ cd ${LIB}-${VERSION}
 
 for ARCH in ${ARCHS}
 do
-    if [ "${ARCH}" == "i386" ];
-    then
-        PLATFORM="iPhoneSimulator"
-        HOST="i386-apple-darwin11"
-    else
-        PLATFORM="iPhoneOS"
-        HOST="${ARCH}-apple-darwin11"
-    fi
+    case "${ARCH}" in
+        "i386"|"x86_64")
+            PLATFORM="iPhoneSimulator"
+            HOST="${ARCH}-apple-darwin11"
+            ;;
+        "arm64")
+            PLATFORM="iPhoneOS"
+            HOST="aarch64-apple-darwin11"
+            ;;
+        *)
+            PLATFORM="iPhoneOS"
+            HOST="${ARCH}-apple-darwin11"
+            ;;
+    esac
 
     SDK="${DEVELOPER}/Platforms/${PLATFORM}.platform/Developer/SDKs/${PLATFORM}${SDKVERSION}.sdk"
 
 #    export CC="clang -arch ${ARCH} -isysroot ${SDK}"
     export CC="clang"
-    export CFLAGS="-arch ${ARCH} -isysroot ${SDK}"
+    export CFLAGS="-arch ${ARCH} -isysroot ${SDK} -miphoneos-version-min=6.0"
     export CXXFLAGS="$CFLAGS"
     export LDFLAGS="$CFLAGS"
     export LD=$CC
@@ -54,10 +60,10 @@ cp -r ${CURRENTPATH}/build/${LIB}/i386/ ${CURRENTPATH}/build/${LIB}/Fat
 rm -rf ${CURRENTPATH}/build/${LIB}/Fat/lib/*
 
 echo "Build library - libspeex.a"
-lipo -create ${CURRENTPATH}/build/${LIB}/i386/lib/lib${LIB}.a ${CURRENTPATH}/build/${LIB}/armv6/lib/lib${LIB}.a ${CURRENTPATH}/build/${LIB}/armv7/lib/lib${LIB}.a -output ${CURRENTPATH}/build/${LIB}/Fat/lib/lib${LIB}.a
+lipo -create ${CURRENTPATH}/build/${LIB}/i386/lib/lib${LIB}.a ${CURRENTPATH}/build/${LIB}/x86_64/lib/lib${LIB}.a ${CURRENTPATH}/build/${LIB}/arm64/lib/lib${LIB}.a ${CURRENTPATH}/build/${LIB}/armv7/lib/lib${LIB}.a -output ${CURRENTPATH}/build/${LIB}/Fat/lib/lib${LIB}.a
 
 echo "Build library - libspeexdsp.a"
-lipo -create ${CURRENTPATH}/build/${LIB}/i386/lib/lib${LIB}dsp.a ${CURRENTPATH}/build/${LIB}/armv6/lib/lib${LIB}dsp.a ${CURRENTPATH}/build/${LIB}/armv7/lib/lib${LIB}dsp.a -output ${CURRENTPATH}/build/${LIB}/Fat/lib/lib${LIB}dsp.a
+lipo -create ${CURRENTPATH}/build/${LIB}/i386/lib/lib${LIB}dsp.a ${CURRENTPATH}/build/${LIB}/x86_64/lib/lib${LIB}dsp.a ${CURRENTPATH}/build/${LIB}/arm64/lib/lib${LIB}dsp.a ${CURRENTPATH}/build/${LIB}/armv7/lib/lib${LIB}dsp.a -output ${CURRENTPATH}/build/${LIB}/Fat/lib/lib${LIB}dsp.a
 
 
 echo "======== CHECK FAT ARCH ========"
